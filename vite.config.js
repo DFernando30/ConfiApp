178e2b80038c4ac938db260ee660ca701027225c
Rubén Velasco <129116167+Velasco182@users.noreply.github.com>
2024-04-14 22:27:34 -0500

    Prueba 2

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    server: {
-      host: '0.0.0.0',
-      strictPort: true,
-      port: 5173,
-      },
     alias: [{ find: "@", replacement: "/src" }],
   },
+  server: {
+    host: '0.0.0.0'
+  }
 });
