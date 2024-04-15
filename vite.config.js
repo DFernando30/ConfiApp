3b06c5d4d4adb636de5aae3f4aa1e84750836c15
Rubén Velasco <129116167+Velasco182@users.noreply.github.com>
2024-04-14 20:07:07 -0500

    Sin Modules 1

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   resolve: {
+    server: {
+      host: true,
+      strictPort: true,
+      port: 8000,
+      },
     alias: [{ find: "@", replacement: "/src" }],
   },
 });
